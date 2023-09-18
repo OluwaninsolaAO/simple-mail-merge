041b6ed5b5ddf167b7702e0e2ee6104a51649093
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-09-18 03:32:37 +0100

    added configuration for color blue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'blue': '#2a30ff',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
