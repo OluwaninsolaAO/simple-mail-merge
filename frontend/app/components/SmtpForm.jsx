aaab998e150e74dbdc08b592362dc866c73c0c2d
MikeRock51 <mikerockmusic51@gmail.com>
2023-09-20 13:47:26 +0100

    Fix smtp switch  Logic

--- frontend/app/components/SmtpForm.jsx
+++ frontend/app/components/SmtpForm.jsx
@@ -29,7 +29,7 @@ export default function SmtpForm() {
       predefined: true,
       username: "gmail",
       alias: "Gmail",
-      password: "",
+      password,
       server: "smtp.gmail.com",
       port: "587",
       rate: "100",
@@ -38,7 +38,7 @@ export default function SmtpForm() {
       predefined: true,
       username: "yahoo",
       alias: "Yahoo",
-      password: "",
+      password,
       server: "smtp.mail.yahoo.com",
       port: "465",
       rate: "100",
@@ -59,23 +59,24 @@ export default function SmtpForm() {
       const selected = smtpConfigurations.find(
         (config) => config.username === selectedConfig
       );
-      if (selected) {
+      if (selected.predefined) {
         setUsername(selected.username);
         setAlias(selected.alias);
-        setPassword(selected.password);
+        setPassword(password);
         setServer(selected.server);
         setPort(selected.port);
         setRate(selected.rate);
       }
-    } else {
-      setUsername("");
-      setAlias("");
-      setPassword("");
-      setServer("");
-      setPort("");
-      setRate("");
+      else {
+        setUsername(username);
+        setAlias(alias);
+        setPassword(password);
+        setServer(server);
+        setPort(port);
+        setRate(rate);
+      }
     }
-  }, [selectedConfig]);
+  }, [selectedConfig, smtpConfig]);
 
   useEffect(() => {
     if (!token) {
@@ -96,7 +97,7 @@ export default function SmtpForm() {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    console.log(config);
+    console.log(smtpConfig);
   }
 
   return (
@@ -113,7 +114,10 @@ export default function SmtpForm() {
             </p>
           )}
           {smtpConfigurations.map((config, index) => (
-            <div className="flex flex-col mb-5" key={index}>
+            <div
+              className="flex flex-col mb-5 rounded bg-indigo-200 p-5"
+              key={index}
+            >
               <div className="flex items-center justify-between mb-2">
                 <div className="flex items-center">
                   <input
@@ -125,12 +129,11 @@ export default function SmtpForm() {
                     onChange={() => {
                       setSelectedConfig(config.username);
                       setConfig(config);
-                      handleChange();
                     }}
                     style={{ width: "16px", height: "16px" }}
                     className="mr-2"
                   />
-                  <h2 className="text-2xl">
+                  <h2 className="text-2xl text-blue">
                     {config.predefined ? config.alias : "Custom Configuration"}
                   </h2>
                 </div>
@@ -157,16 +160,7 @@ export default function SmtpForm() {
               )}
             </div>
           ))}
-          <div className="mt-2">
-            <button
-              className="w-full bg-blue py-3 text-center text-white hover:bg-white hover:text-blue border hover:border-blue transition ease-out duration-300"
-              onClick={handleSubmit}
-            >
-              Save SMTP Configuration
-            </button>
-          </div>
         </div>
-        {/* <CustomSMTPForm /> */}
         <div className="w-full lg:w-1/2 py-16 pt-8 px-12 text-center">
           {/* The user will be able to set up a new SMTP configuration here */}
           <h2 className="text-3xl mb-4">Custom SMTP Configuration</h2>
@@ -177,9 +171,13 @@ export default function SmtpForm() {
                 placeholder="User Name"
                 id="username"
                 value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                  setConfig({ ...smtpConfig, username: e.target.value });
+                }}
                 required
                 className="border border-blue py-1 px-2 w-full"
+                disabled={smtpConfig && smtpConfig.predefined ? true : false}
               />
             </div>
             <div className="mt-2">
@@ -188,9 +186,13 @@ export default function SmtpForm() {
                 placeholder="Alias"
                 id="alias"
                 value={alias}
-                onChange={(e) => setAlias(e.target.value)}
+                onChange={(e) => {
+                  setAlias(e.target.value);
+                  setConfig({ ...smtpConfig, alias: e.target.value });
+                }}
                 required
                 className="border border-blue py-1 px-2 w-full"
+                disabled={smtpConfig && smtpConfig.predefined ? true : false}
               />
             </div>
             <div className="mt-2">
@@ -199,7 +201,10 @@ export default function SmtpForm() {
                 placeholder="Password"
                 id="password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setConfig({ ...smtpConfig, password: e.target.value });
+                }}
                 required
                 className="border border-blue py-1 px-2 w-full"
               />
@@ -210,9 +215,13 @@ export default function SmtpForm() {
                 placeholder="SMTP Server"
                 id="smtpServer"
                 value={server}
-                onChange={(e) => setServer(e.target.value)}
+                onChange={(e) => {
+                  setServer(e.target.value);
+                  setConfig({ ...smtpConfig, server: e.target.value });
+                }}
                 required
                 className="border border-blue py-1 px-2 w-full"
+                disabled={smtpConfig && smtpConfig.predefined ? true : false}
               />
             </div>
             <div className="mt-2">
@@ -221,7 +230,10 @@ export default function SmtpForm() {
                 placeholder="SMTP Port"
                 id="smtpPort"
                 value={port}
-                onChange={(e) => setPort(e.target.value)}
+                onChange={(e) => {
+                  setPort(e.target.value);
+                  setConfig({ ...smtpConfig, port: e.target.value });
+                }}
                 required
                 className="border border-blue py-1 px-2 w-full"
                 disabled={smtpConfig && smtpConfig.predefined ? true : false}
@@ -233,7 +245,10 @@ export default function SmtpForm() {
                 placeholder="SMTP Rate"
                 id="smtpRate"
                 value={rate}
-                onChange={(e) => setRate(e.target.value)}
+                onChange={(e) => {
+                  setRate(e.target.value);
+                  setConfig({ ...smtpConfig, rate: e.target.value });
+                }}
                 required
                 disabled={smtpConfig && smtpConfig.predefined ? true : false}
                 className="border border-blue py-1 px-2 w-full"
