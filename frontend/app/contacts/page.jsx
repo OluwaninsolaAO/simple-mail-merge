808aaae7fb10418d74f423f0ae6256dfc642ba10
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-09-21 04:02:17 +0100

    implemented import contacts functionality

---
+++ frontend/app/contacts/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ImportContacts from "../components/ImportContacts";
+
+function ContactsPage() {
+  return (
+    <ImportContacts />
+  );
+}
+
+export default ContactsPage;
